6ccc28edecd4e16b4b35b0c932eb183558ff434e
Sarah Cooley <scooley@microsoft.com>
2016-10-26 16:46:51 -0700

    added more stuff

--- py_stackapi/hvdata.py
+++ py_stackapi/hvdata.py
@@ -1,6 +1,24 @@
-from stackapi import StackAPI
+from stackapi import StackAPI, StackAPIError
+from datetime import datetime, timedelta, date
 
-site = StackAPI('stackoverflow')
-comments = site.fetch('comments')
+try:
+	site = StackAPI('stackoverflow')
+except StackAPIError as e:
+	print ("Error message: %s" % (e.message)) 
 
-print comments
+today = date.today()
+year = timedelta(days=365)
+
+# Questions related to Hyper-V
+
+years = range(2016, 2005, -1)
+
+tod = today
+fromd = today - year
+
+for y in years:
+	questions = site.fetch('questions', tagged='hyper-v', fromdate=fromd, todate=tod, sort='votes')
+	print '{0:5d} questions in {1}'.format(len(questions['items']), fromd)
+	tod = tod - year
+	fromd = fromd - year
+
